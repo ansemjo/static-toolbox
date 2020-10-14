2c39a3729f16d5b7c73e4bfcc46cf74b67ade720
Anton Semjonov <anton@semjonov.de>
2020-10-14 11:23:30 +0200

    add a release script to generate binaries and sources

---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# compile all binaries and package some metadata for publishing
+set -eu
+
+# clean directory
+git clean -idx
+
+# compile all binaries
+#make all
+#make sources
+
+# calculate checksums
+sha256sum $(ls | grep -vE '(build|Makefile|README.md|release.sh)') | tee CHECKSUMS.sha256
+
+# output versions to a text file
+for util in $(cd build/ && ls | grep -v '.keys'); do
+  case $util in
+    busybox) ./$util | head -1;;
+    zstd) ./zstd -V | sed 's/ \?\*\+ \?//g';;
+    *) ./$util --version | head -1;;
+  esac
+done | tee VERSIONS
